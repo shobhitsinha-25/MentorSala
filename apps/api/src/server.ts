a452412cc1024171dea15991900b75abbfe41d37
shobhitsinha-25 <sinhashobhit025@gmail.com>
2026-06-13 10:07:36 +0530

    Add Google Analytics

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,8 +1,9 @@
-import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
-console.log(process.env.DAtABASE_URL)
-const PORT=process.env.PORT || 5000;
+
+import app from "./app";
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
